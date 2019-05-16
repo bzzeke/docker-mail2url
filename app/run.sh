f0da969eff85c96fa32a253ab0756ae1764092b0
Ilya Shalnev <zeke.home@gmail.com>
2019-05-16 11:09:29 +0000

    [~] ENV var name was changed

--- app/run.sh
+++ app/run.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/xx_log_server_xx/${LOG_SERVER}/g" /etc/rsyslog.conf
 
-postconf -e myhostname=$POST_HOST
+postconf -e myhostname=$ROOT_SERVER_NAME
 
 echo "$POST_EMAIL	curl_email" >> /etc/postfix/redirect
 echo "curl_email: \"|curl --data-binary @- $POST_URL\"" > /etc/aliases
